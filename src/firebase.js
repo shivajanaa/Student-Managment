0c7e7071c5ec85cd0ab94d1ab779c1e1828cc4b4
Shiva <sjana0484@gmail.com>
2025-05-14 15:27:18 +0530

    fixed api security issue

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-     apiKey: "AIzaSyDDNnCVWpJkGTHToFyJ573qzTzBCc-Bx0Y",
-  authDomain: "student-manager-app-6b6fe.firebaseapp.com",
-  projectId: "student-manager-app-6b6fe",
-  storageBucket: "student-manager-app-6b6fe.firebasestorage.app",
-  messagingSenderId: "996390617720",
-  appId: "1:996390617720:web:8b49e0abb4200b245ca38d" };
+import firebaseConfig from "./firebaseConfig";
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const firestoreDatabase = getFirestore(app);
 
-export { db };
+export default firestoreDatabase;
